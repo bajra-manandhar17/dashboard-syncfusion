6b58efab4fe4c6b254d75e3e73985a84dea133d0
Bajra Manandhar <32059486+bajra-manandhar17@users.noreply.github.com>
2022-06-10 19:38:19 +0545

    Sidebar created [No context]

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ import { Ecommerce, Orders, Calendar, Employees, Customers, Kanban, Editor, Colo
 import './App.css';
 
 const App = () => {
-    const activeMenu = false;
+    const activeMenu = true;
 
     return (
         <div>
             <BrowserRouter>
-                <div className='flex relative dark:bg-main-dark-bg'>
+                <div className='relative flex dark:bg-main-dark-bg'>
                     <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
                         <TooltipComponent content='Settings' position='Top'>
-                            <button type='bottom' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{ background: 'green', borderRadius: '50%' }}>
+                            <button type='bottom' className='p-3 text-3xl text-white hover:drop-shadow-xl hover:bg-light-gray' style={{ background: 'green', borderRadius: '50%' }}>
                                 <FiSettings />
                             </button>
                         </TooltipComponent>
                     </div>
 
                     {activeMenu ? (
-                        <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+                        <div className='fixed bg-white w-72 sidebar dark:bg-secondary-dark-bg'>
                             <Sidebar />
                         </div> ): (
                         <div className='w-0 dark:bg-secondary-dark-bg' >
@@ -31,10 +31,12 @@ const App = () => {
                         </div>
                     )}
                     <div className={ 
-                        `dark:bg-main-bg bg-main-bg min-h-screen w-full ${ activeMenu ? 'md:ml-72' : 'flex-2' }`
+                       activeMenu
+                       ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                       : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
                         }>
-                            <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-                            <Navbar />
+                            <div className='fixed w-full md:static bg-main-bg dark:bg-main-dark-bg navbar'>
+                                <Navbar />
                             </div>
                     </div>
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { SiShopware } from 'react-icons/si';
+import { MdOutlineCancel } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import { links } from '../data/dummy';
 
 const Sidebar = () => {
+  const activeMenu = true;
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
+  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
+
   return (
-    <div>Sidebar</div>
+    <div className='h-screen pb-10 ml-3 overflow-auto md:overflow-hidden md:hover:overflow-auto'>
+      {activeMenu && (
+        <>
+          <div className="flex items-center justify-between">
+            <Link to='/' onClick={() => {}} className='flex items-center gap-3 mt-4 ml-3 text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
+              <SiShopware /> <span>Shopyy</span>
+            </Link>
+            <TooltipComponent content='Menu' position='BottomCenter'>
+              <button type='button' onClick={() => {}} className='block p-3 mt-4 text-xl rounded-full hover:bg-light-gray md:hidden'>
+                <MdOutlineCancel />
+              </button>
+            </TooltipComponent>
+          </div>
+          <div className='mt-10'>
+            {links.map((item) => (
+              <div key={item.title}>
+                <p className='m-3 mt-4 text-gray-400 uppercase'>
+                  {item.title}
+                </p>
+                {item.links.map((link) => (
+                  <NavLink key={link.title} to={`/${link.name}`} onclick={() => {}} className={({ isActive }) => isActive ? activeLink : normalLink}>
+                    {link.icon}
+                    <span className='capitalize'>{link.name}</span>
+                  </NavLink>
+                ))}
+              </div>
+            ))}
+          </div>
+
+        </>  
+      )}
+    </div>
   )
 }
 
