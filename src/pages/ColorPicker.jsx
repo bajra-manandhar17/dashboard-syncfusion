e7577ee5e2f9ba3b1693300f0d88176346e7be3d
Bajra Manandhar <32059486+bajra-manandhar17@users.noreply.github.com>
2022-06-19 13:16:58 +0545

    Color Picker

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
+
+import { Header } from '../components';
+
+const changePreview = (args) => {
+  document.getElementById('preview').style.backgroundColor = args.currentValue.hex;
+}
+
+const CustomColorPicker = ({id, mode}) => <ColorPickerComponent id={id} mode={mode} modeSwitcher={false} showButtons={false} inline change={changePreview} />
 
 const ColorPicker = () => {
   return (
-    <div>ColorPicker</div>
+    <div className="p-2 m-2 mt-24 bg-white md:m-10 md:p-10 rounded-3xl">
+    <Header category="App" title="Color Picker" />
+    <div className="text-center">
+      <div id="preview" />
+      <div className="flex flex-wrap items-center justify-center gap-20">
+        <div>
+          <p className='mt-2 mb-4 text-2xl font-semibold'>Inline Palete</p>
+          <CustomColorPicker id="inline-pallete" mode="Palette" />
+        </div>
+        <div>
+          <p className='mt-2 mb-4 text-2xl font-semibold'>Inline Picker</p>
+          <CustomColorPicker id="inline-pallete" mode="Picker" />
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
 
